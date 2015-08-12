97c30b1760ce64f2da9e98c3fbc2863df9ecacd2
Michael Grünewald <michipili@gmail.com>
2015-08-12 11:22:43 +0200

    Support autoinstall of bmake and opam for Travis CI

---
+++ subr/anvil_travisci_autoinstall.sh
@@ -0,0 +1,114 @@
+#!/bin/sh
+
+### autoinstall -- Autoinstall dependencies for Travis CI
+
+# Anvil (https://github.com/michipili/anvil)
+# This file is part of Anvil
+#
+# Copyright © 2015 Michael Grünewald
+#
+# This file must be used under the terms of the CeCILL-B.
+# This source file is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt
+
+: ${prefix:=${HOME}/.local}
+: ${srcdir:=${HOME}/.local/sources}
+: ${autoinstall_bsdowl_version:=master}
+
+
+autoinstall_bmake()
+{
+    install -d "${srcdir}"
+    cd "${srcdir}"
+
+    curl -s http://michipili.github.io/bsdowl/src/bmake-20150606.tar.gz\
+        | tar xzfC - "${srcdir}"
+
+    ./bmake/boot-strap --prefix="${prefix}" --install
+    ./bmake/boot-strap --prefix="${prefix}" op=install
+}
+
+autoinstall_bsdowl()
+{
+    install -d "${srcdir}"
+    cd "${srcdir}"
+
+    git clone https://github.com/michipili/bsdowl
+    cd bsdowl
+    git checkout "${autoinstall_bsdowl_version}"
+    autoconf
+    ./configure --prefix="${prefix}"
+    bmake build
+    bmake install
+}
+
+
+autoinstall_opam()
+{
+    local method package url
+
+    set -a
+    OPAMYES=1
+    OPAMVERBOSE=1
+    set +a
+
+    opam init
+    eval $(opam config env)
+
+    while IFS='|' read method package url; do
+        autoinstall_opam__${method} "${package}" "${url}"
+    done < ./.travis.opam
+}
+
+autoinstall_opam__repository()
+{
+    opam install "$1"
+}
+
+autoinstall_opam__git()
+{
+    set -a
+    OPAMYES=1
+    OPAMVERBOSE=1
+    set +a
+
+    (cd "${srcdir}" && git clone "$2" "$1")\
+        && opam pin add "$1" "${srcdir}/$1"
+}
+
+autoinstall_usage()
+{
+    iconv -c -f utf-8 <<EOF
+Usage: autoinstall [-h][-s SRCDIR] [-p PREFIX] bsdowl bmake
+ Install dependencies
+Options:
+ -h Display this help message.
+ -p PREFIX [${prefix}]
+    Use PREFIX as installation prefix.
+ -s SRCDIR [${srcdir}]
+    Download sources to SRCDIR.
+EOF
+}
+
+while getopts "hs:p:" OPTION; do
+    case "${OPTION}" in
+        h)	autoinstall_usage; exit 0;;
+        s)	srcdir="${OPTARG}";;
+        p)	prefix="${OPTARG}";;
+        ?)	autoinstall_usage; exit 64;;
+    esac
+done
+shift $(expr $OPTIND - 1)
+for package in "$@"; do
+    case "${package}" in
+        bmake|bsdowl|opam);;
+        *) autoinstall_usage; exit 64;;
+    esac
+done
+for package in "$@"; do
+    ( autoinstall_${package} )
+done
+
+### End of file `autoinstall'
